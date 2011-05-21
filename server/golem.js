27c38a21034d882d33d4d063ea8fbd4a94a371ec
Rob Speer <rspeer@mit.edu>
2011-05-21 04:59:54 -0400

    add roadmap for what to do next

--- server/golem.js
+++ server/golem.js
@@ -2,6 +2,16 @@ var exec = require("child_process").exec;
 
 function gainHandler(request, responder, query) {
   responder.fail("Not implemented");
+  // Determine the set of available cards / sets of cards.
+  //   (Get this from tableau / cost.)
+  //
+  // For each possibility:
+  //   Add those cards to the current hand.
+  //   Update VP, card count, etc. accordingly.
+  //   Normalize to cards per hand.
+  //   Convert to a VW string.
+  //
+  // Run the file through VW. Sort the results. Do the best thing.
 }
 exports.gain = gainHandler;
 
