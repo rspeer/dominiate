4e0440a1afc7af20ea8d1af6905a6f1bb287e1ce
Rob Speer <rspeer@mit.edu>
2011-05-26 14:39:05 -0400

    make vowpal pipe return an error when vw fails

--- server/vowpal-pipe.sh
+++ server/vowpal-pipe.sh
@@ -3,6 +3,5 @@
 # Runs standard input through Vowpal Wabbit in test mode, then sends the
 # results to standard output. Requires one argument, which is the name of the
 # existing VW model to use.
-cat > vw_input.txt
-vw -t -d vw_input.txt --adaptive -q cc -q oo -q uu -q vv -q co -q cu -q cv -q ou -q ov --loss_function logistic -i $1 -p vw_output.txt
-cat vw_output.txt
+cat > /tmp/vw_input.txt
+vw -t -d /tmp/vw_input.txt --adaptive -q cc -q oo -q uu -q vv -q co -q cu -q cv -q ou -q ov --loss_function logistic -i $1 -p /tmp/vw_output.txt && cat /tmp/vw_output.txt
