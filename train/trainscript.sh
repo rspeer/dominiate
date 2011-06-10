090a9531e0498bd26f1bb07d2c700bb952f1395f
Rob Speer <rspeer@mit.edu>
2011-06-10 19:52:49 -0400

    fix thinko in training script

--- train/trainscript.sh
+++ train/trainscript.sh
@@ -6,6 +6,7 @@ vw -d turn1.rand.txt -q cc -q oo -q co -q cs -q os -b 24 -f model1.vw --loss_fun
 
 for i in `seq 2 25`
 do
-  vw -d turn$i.rand.txt -q cc -q oo -q co -q cs -q os -b 24 -i model -f model$(($i-1)).vw model$i.vw --loss_function logistic --adaptive -l 0.1
+  vw -d turn$i.rand.txt -q cc -q oo -q co -q cs -q os -b 24\
+  -i model$(($i-1)).vw -f model$i.vw --loss_function logistic --adaptive -l 0.1
 done
 
