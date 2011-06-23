e700bdfa74fd3db38ca7869e0d7065c08c95390d
Rob Speer <rspeer@mit.edu>
2011-06-23 17:45:41 -0400

    trained a 22-bit model

--- server/vowpal-pipe.sh
+++ server/vowpal-pipe.sh
@@ -4,6 +4,5 @@
 # results to standard output. Requires one argument, which is the name of the
 # existing VW model to use.
 #
-# TODO: change to 24-bit model
 cat > /tmp/vw-input-$$.txt
-vw -t -d /tmp/vw-input-$$.txt --adaptive -q cc -q oo -q co -q cs -q os --loss_function logistic -i $1 -p /tmp/vw-output-$$.txt && cat /tmp/vw-output-$$.txt && rm /tmp/vw-input-$$.txt && rm /tmp/vw-output-$$.txt
+vw -t -d /tmp/vw-input-$$.txt -q cc -q oo -q co -q cs -q os -b 22 --loss_function logistic -i $1 -p /tmp/vw-output-$$.txt -a > /tmp/vw-audit-$$.txt && cat /tmp/vw-output-$$.txt && rm /tmp/vw-input-$$.txt && rm /tmp/vw-output-$$.txt
--- train/trainscript.sh
+++ train/trainscript.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 cd output
 
+#vw -d turn1.rand.txt -q cc -q oo -q co -q cs -q os -b 22 -f model$i.vw \
+#--loss_function logistic -c --passes 2
+
 for i in `seq 1 25`
 do
-vw -d turn$i.rand.txt -q cc -q oo -q co -q cs -q os -b 20 -f model$i.vw --loss_function logistic
+vw -d turn$i.rand.txt -q cc -q oo -q co -q cs -q os -b 22 -f model$i.vw \
+--loss_function logistic -c --passes 2
 done
 
