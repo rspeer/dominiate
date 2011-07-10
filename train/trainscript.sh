614c497622fd8f4451bb4591953802d130b1a32e
Rob Speer <rspeer@mit.edu>
2011-07-10 11:36:36 -0400

    new training script for new version of vw

--- train/trainscript.sh
+++ train/trainscript.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 cd output
 
-#vw -d turn1.rand.txt -q cc -q oo -q co -q cs -q os -b 22 -f model$i.vw \
-#--loss_function logistic -c --passes 2
-
 for i in `seq 1 25`
 do
 vw -d turn$i.rand.txt -q cc -q oo -q co -q cs -q os -b 22 -f model$i.vw \
---loss_function logistic -c --passes 2
+--loss_function logistic --adaptive -c --passes 2
 done
 
+# note -- currently needs to be re-run for turns 13 and 16
